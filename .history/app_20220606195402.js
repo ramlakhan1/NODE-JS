f4cbf8116b85742a0f8d6e5e89ff5d5cc89f037f
RAM LAKHAN SINGH <pt2867000@gmail.com>
2022-06-09 16:25:55 +0530

    DAY 1 NODE JS

---
+++ .history/app_20220606195402.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+    if(req.url === '/api/courses') {
+})
+
+server.listen(3000,() => {
+    console.log('Server is running on port 3000');
+});
